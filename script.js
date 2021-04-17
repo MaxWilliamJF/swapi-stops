0ce9d63f7c16ae9cbbbfdf47c20c4ae70a995e09
Max Grigório Damasceno <maxwilljf@gmail.com>
2021-04-16 23:46:40 -0300

    Page 'title' and now it calculates (without consumables)

--- script.js
+++ script.js
@@ -4,34 +4,44 @@ const API = {
 
 let starships = [];
 
-const setStarships = (starships) => starships;
-
 const validateDistance = (distance) => {
     return distance;
 }
 
-const calculateStops = () => {
+const start = async () => {
     const distance = validateDistance(document.getElementById('distance').value);
     console.log('Future validated distance', distance);
+
     if (distance) {
-        findStarhips();
+        await findStarhips();
+        calculateStops(starships, distance);
     }
 }
 
-const findStarhips = () => {
+const calculateStops = (starships, distance) => {
+    const qtdStarships = starships.length;
+
+    for (let i = 0; i < qtdStarships; i++) {
+        const starship = starships[i];
+        const stops = Math.ceil(distance / Number(starship.MGLT));
+        console.log(starship.MGLT + ' - ' + starship.name, '\n' + stops + '\n');
+    }
+}
+
+const findStarhips = async () => {
     if (!starships.length) {
+
         // Get first ten starships
-        fetch(API.baseURL + 'starships')
+        await fetch(API.baseURL + 'starships')
             .then(response => response.json())
             .then(data => {
                 if (data.results) {
+                    // Create setter?
                     starships = data.results;
                 }
             });
     }
-
-    return starships;
 }
 
 const btnCalculate = document.getElementById('calculate');
-btnCalculate.addEventListener('click', calculateStops);
+btnCalculate.addEventListener('click', start);
